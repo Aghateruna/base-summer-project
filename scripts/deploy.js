ad67f55a288e605e716ed1c1f127ecb042b384f1
Agha teruna <94300183+Aghateruna@users.noreply.github.com>
2025-08-16 03:00:54 +0000

    Save project progress without .env

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,13 +1,21 @@
 const hre = require("hardhat");
 
 async function main() {
-  const Counter = await hre.ethers.getContractFactory("Counter");
-    const counter = await Counter.deploy();
-      await counter.deployed();
-        console.log("Counter deployed to:", counter.address);
-        }
+  const [deployer] = await hre.ethers.getSigners();
+    console.log("Deploying with account:", deployer.address);
 
-        main().catch((error) => {
-          console.error(error);
-            process.exitCode = 1;
-            });
+      const CounterFactory = await hre.ethers.getContractFactory("Counter");
+        const counter = await CounterFactory.deploy();
+
+          // Ethers v6:
+            await counter.waitForDeployment();
+
+              console.log("Counter deployed to:", await counter.getAddress());
+              }
+
+              main()
+                .then(() => process.exit(0))
+                  .catch((err) => {
+                      console.error(err);
+                          process.exit(1);
+                            });
